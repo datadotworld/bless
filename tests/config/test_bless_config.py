ef0973811d41d7a8bef740153e0a8baba393c4e5
Adam McElwee <adam@mcelwee.me>
2018-11-29 16:54:03 -0600

    Merge upstream changes (#2)
    * Allows username validation against IAM groups
    
    This change gives the option to validate the remote username against
    the IAM groups containing the user invoking the lambda function. This
    is an optional feature which is used in conjunction with kmsauth.
    
    For example, if there were two groups of users, you could put your
    admins in the ssh-admin IAM group to allow them to generate certificates
    with a remote_username of 'admin'. Users with fewer permissions could be
    in the ssh-user group to allow them to generate certificates for the 'user'
    account.
    
    The group name is configurable, however they must all be in a consistent
    format, and must all contain the relevant remote_username once.
    
    * Compressed CA private key support
    
    * Fixing https://github.com/Netflix/bless/issues/72 thanks @Immortalin and @tuxinaut .
    
    * Add support for loading ED25519 public keys
    
    * Add certificate builder and test ED25519 signed by RSA
    
    * Allowing BLESS lambda to accept ed25519 keys, completing https://gith… (#74)
    
    * Allowing BLESS lambda to accept ed25519 keys, completing https://github.com/Netflix/bless/pull/71 .  Thanks @jnewbigin .
    
    * Moving BLESS to python 3.6. (#75)
    
    * Moving BLESS to python 3.6.
    You just need to rebuild, publish, and switch your lambda runtime from 2.7 to 3.6.
    
    * Moving TravisCI to Python3.6 as well.
    
    * bless_client.py: fix argv unpacking when using a kmsauth token (#63)
    
    * Add the FileSync flag to the zip command (#76)
    
    * Make lambda_configs dir optional for publish make target (#69)
    
    * Adding a blacklisted remote_usernames option.  This would prevent particular SSH Authorized Principals from being included in a BLESS certificate.
    
    * Refactored BLESS to cache KMS decrypt results for the ca private key password.
    
    * Bumping to Release v.0.3.0
    
    Features include:
    Python 3.6 Lambda support
    Caching of the KMS decrypted CA Private Key Password.
    Compressed CA Private Key support, allowing RSA 4096 keys to be set in the Lambda Environment.
    Issue certificates for ED25519 public keys (RSA CA).
    New option to validate the remote username against the IAM groups of the calling user.
    Updated dependencies.

--- tests/config/test_bless_config.py
+++ tests/config/test_bless_config.py
@@ -1,6 +1,6 @@
 import base64
 import os
-
+import zlib
 import pytest
 
 from bless.config.bless_config import BlessConfig, \
@@ -23,7 +23,9 @@ from bless.config.bless_config import BlessConfig, \
     CERTIFICATE_EXTENSIONS_OPTION, \
     USERNAME_VALIDATION_OPTION, \
     USERNAME_VALIDATION_DEFAULT, \
-    REMOTE_USERNAMES_VALIDATION_OPTION
+    REMOTE_USERNAMES_VALIDATION_OPTION, \
+    CA_PRIVATE_KEY_COMPRESSION_OPTION, \
+    CA_PRIVATE_KEY_COMPRESSION_OPTION_DEFAULT
 
 
 def test_empty_config():
@@ -35,12 +37,75 @@ def test_config_no_password():
     with pytest.raises(ValueError) as e:
         BlessConfig('bogus-region',
                     config_file=os.path.join(os.path.dirname(__file__), 'full.cfg'))
-    assert 'No Region Specific And No Default Password Provided.' == e.value.message
+    assert 'No Region Specific And No Default Password Provided.' == str(e.value)
 
     config = BlessConfig('bogus-region',
                          config_file=os.path.join(os.path.dirname(__file__), 'full-with-default.cfg'))
     assert '<INSERT_DEFAULT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>' == config.getpassword()
 
+def test_wrong_compression_env_key(monkeypatch):
+    extra_environment_variables = {
+        'bless_ca_default_password': '<INSERT_DEFAULT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
+        'bless_ca_ca_private_key_compression': 'lzh',
+        'bless_ca_ca_private_key': str(base64.b64encode(b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>'), encoding='ascii')
+    }
+
+    for k, v in extra_environment_variables.items():
+        monkeypatch.setenv(k, v)
+
+    # Create an empty config, everything is set in the environment
+    config = BlessConfig('us-east-1', config_file='')
+
+    with pytest.raises(ValueError) as e:
+        config.getprivatekey()
+
+    assert "Compression lzh is not supported." == str(e.value)
+
+def test_none_compression_env_key(monkeypatch):
+    extra_environment_variables = {
+        'bless_ca_default_password': '<INSERT_DEFAULT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
+        'bless_ca_ca_private_key_compression': 'none',
+        'bless_ca_ca_private_key': str(base64.b64encode(b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>'), encoding='ascii')
+    }
+
+    for k, v in extra_environment_variables.items():
+        monkeypatch.setenv(k, v)
+
+    # Create an empty config, everything is set in the environment
+    config = BlessConfig('us-east-1', config_file='')
+
+    assert b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>' == config.getprivatekey()
+
+def test_zlib_positive_compression(monkeypatch):
+    extra_environment_variables = {
+        'bless_ca_default_password': '<INSERT_DEFAULT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
+        'bless_ca_ca_private_key_compression': 'zlib',
+        'bless_ca_ca_private_key': str(base64.b64encode(zlib.compress(b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>')), encoding='ascii')
+    }
+
+    for k, v in extra_environment_variables.items():
+        monkeypatch.setenv(k, v)
+
+    # Create an empty config, everything is set in the environment
+    config = BlessConfig('us-east-1', config_file='')
+
+    assert b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>' == config.getprivatekey()
+
+def test_zlib_compression_env_with_uncompressed_key(monkeypatch):
+    extra_environment_variables = {
+        'bless_ca_default_password': '<INSERT_DEFAULT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
+        'bless_ca_ca_private_key_compression': 'zlib',
+        'bless_ca_ca_private_key': base64.b64encode(b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>'),
+    }
+
+    for k, v in extra_environment_variables.items():
+        monkeypatch.setenv(k, v)
+
+    # Create an empty config, everything is set in the environment
+    config = BlessConfig('us-east-1', config_file='')
+
+    with pytest.raises(zlib.error) as e:
+        config.getprivatekey()
 
 def test_config_environment_override(monkeypatch):
     extra_environment_variables = {
@@ -56,7 +121,7 @@ def test_config_environment_override(monkeypatch):
         'bless_ca_us_east_1_password': '<INSERT_US-EAST-1_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
         'bless_ca_default_password': '<INSERT_DEFAULT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
         'bless_ca_ca_private_key_file': '<INSERT_YOUR_ENCRYPTED_PEM_FILE_NAME>',
-        'bless_ca_ca_private_key': base64.b64encode('<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>'),
+        'bless_ca_ca_private_key': str(base64.b64encode(b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>'), encoding='ascii'),
 
         'kms_auth_use_kmsauth': 'True',
         'kms_auth_kmsauth_key_id': '<INSERT_ARN>',
@@ -80,7 +145,7 @@ def test_config_environment_override(monkeypatch):
 
     assert '<INSERT_US-EAST-1_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>' == config.getpassword()
     assert '<INSERT_YOUR_ENCRYPTED_PEM_FILE_NAME>' == config.get(BLESS_CA_SECTION, CA_PRIVATE_KEY_FILE_OPTION)
-    assert '<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>' == config.getprivatekey()
+    assert b'<INSERT_YOUR_ENCRYPTED_PEM_FILE_CONTENT>' == config.getprivatekey()
 
     assert config.getboolean(KMSAUTH_SECTION, KMSAUTH_USEKMSAUTH_OPTION)
     assert '<INSERT_ARN>' == config.get(KMSAUTH_SECTION, KMSAUTH_KEY_ID_OPTION)
@@ -92,26 +157,29 @@ def test_config_environment_override(monkeypatch):
 
 @pytest.mark.parametrize(
     "config,region,expected_cert_valid,expected_entropy_min,expected_rand_seed,expected_log_level,"
-    "expected_password,expected_username_validation", [
+    "expected_password,expected_username_validation,expected_key_compression", [
         ((os.path.join(os.path.dirname(__file__), 'minimal.cfg')), 'us-west-2',
          CERTIFICATE_VALIDITY_SEC_DEFAULT, ENTROPY_MINIMUM_BITS_DEFAULT, RANDOM_SEED_BYTES_DEFAULT,
          LOGGING_LEVEL_DEFAULT,
          '<INSERT_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
-         USERNAME_VALIDATION_DEFAULT
+         USERNAME_VALIDATION_DEFAULT,
+         CA_PRIVATE_KEY_COMPRESSION_OPTION_DEFAULT
          ),
-        ((os.path.join(os.path.dirname(__file__), 'full.cfg')), 'us-west-2',
+        ((os.path.join(os.path.dirname(__file__), 'full-zlib.cfg')), 'us-west-2',
          1, 2, 3, 'DEBUG',
          '<INSERT_US-WEST-2_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
-         'debian'
+         'debian',
+         'zlib'
          ),
         ((os.path.join(os.path.dirname(__file__), 'full.cfg')), 'us-east-1',
          1, 2, 3, 'DEBUG',
          '<INSERT_US-EAST-1_KMS_ENCRYPTED_BASE64_ENCODED_PEM_PASSWORD_HERE>',
-         'debian'
+         'debian',
+         'zlib'
          )
     ])
 def test_configs(config, region, expected_cert_valid, expected_entropy_min, expected_rand_seed,
-                 expected_log_level, expected_password, expected_username_validation):
+                 expected_log_level, expected_password, expected_username_validation, expected_key_compression):
     config = BlessConfig(region, config_file=config)
     assert expected_cert_valid == config.getint(BLESS_OPTIONS_SECTION,
                                                 CERTIFICATE_VALIDITY_BEFORE_SEC_OPTION)
@@ -126,3 +194,5 @@ def test_configs(config, region, expected_cert_valid, expected_entropy_min, expe
     assert expected_password == config.getpassword()
     assert expected_username_validation == config.get(BLESS_OPTIONS_SECTION,
                                                       USERNAME_VALIDATION_OPTION)
+    assert expected_key_compression == config.get(BLESS_CA_SECTION,
+                                                  CA_PRIVATE_KEY_COMPRESSION_OPTION)
